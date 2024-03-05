9e8ec046c92efe522390ddc4ddaf54057b2f12f8
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-03-05 11:22:46 -0600

    commit

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { IProduct } from './product.model';
-import { CartService } from '../cart.service';
 import { ProductService } from './product.service';
+import { CartService } from '../cart/cart.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'bot-catalog',
@@ -15,12 +16,17 @@ export class CatalogComponent {
 
   constructor(
     private cartSvc: CartService,
-    private productSvc: ProductService
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
     this.productSvc.getProducts().subscribe((products) => {
       this.products = products;
+      this.route.queryParams.subscribe((params) => {
+        this.filter = params['filter'] ?? '';
+      });
     });
   }
 
@@ -34,5 +40,6 @@ export class CatalogComponent {
 
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 }
